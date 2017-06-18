15b88cee03502d01f4c4ac4ed1fd1f110a08adf8
Jonatas Senna <jonrsp96@gmail.com>
2017-06-18 15:56:48 -0300

    tentando recursão

--- LFCFDLazyTestes.hs
+++ LFCFDLazyTestes.hs
@@ -6,6 +6,9 @@ import Test.HUnit
 
 v5 = Valor 5
 
+
+exp1 = Lambda "x" exp1
+
 let1 = Let "x" (Valor 5) (Lambda "y" (Soma (Ref "x") (Ref "y")))
 
 -- let x = 5 in (\y -> x+y)3
@@ -26,23 +29,28 @@ let7 = Let "x" (Valor 2) (Let "y" (Soma(Valor 4)(Ref "x")) (Ref "x"))
 
 let8 = Let "x" (Subtracao (Valor 4)(Valor 1)) (Let "y" (Multiplicacao(Ref "x")(Ref "x")) (Ref "y"))
 
-teste1 = TestCase (assertEqual "avaliar 5" (VInt 5) (avaliar v5 [])) --ok
 
-teste2 = TestCase (assertEqual "avaliar let x = 5 in (\\y -> x + y) 3" (VInt 8) (avaliar aplicacao [])) --ok
---myTests
-teste3 = TestCase (assertEqual "avaliar let x = 3 in x+1" (VInt 4) (avaliar let2 [])) --ok
+
+teste1 = TestCase (assertEqual "avaliar 5" (VInt 5) (avaliar v5 [])) 
+
+teste2 = TestCase (assertEqual "avaliar let x = 5 in (\\y -> x + y) 3" (VInt 8) (avaliar aplicacao [])) 
+
+teste3 = TestCase (assertEqual "avaliar let x = 3 in x+1" (VInt 4) (avaliar let2 [])) 
 
 teste4 = TestCase (assertEqual "avaliar let x = 4+5 in let y = x+x in let z = y in let x = 4 in z" (VInt 18) (avaliar let3 []))
 
 teste5 = TestCase (assertEqual "avaliar let x= 3 in x" (VInt 3) (avaliar let4 []))
 
-teste6 = TestCase (assertEqual "avaliar let x=4 in x+x" (VInt 8) (avaliar let5 [])) --ok
+teste6 = TestCase (assertEqual "avaliar let x=4 in x+x" (VInt 8) (avaliar let5 [])) 
 
-teste7 = TestCase (assertEqual "avaliar let x=3+4 in x+x" (VInt 14) (avaliar let6 []) ) --ok
+teste7 = TestCase (assertEqual "avaliar let x=3+4 in x+x" (VInt 14) (avaliar let6 []) ) 
 
 teste8 = TestCase (assertEqual "avaliar let x=2 in let y = 4 + x in x" (VInt 2) (avaliar let7 []) )
 
 teste9 = TestCase (assertEqual "avaliar let x=4-1 in let y=x*x in y" (VInt 9) (avaliar let8 []) )
+
+
+
 
 todosOsTestes = TestList [ teste1
                          , teste2
